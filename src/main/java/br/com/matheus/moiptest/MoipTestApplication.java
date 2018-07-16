5b752b5af3c332ab3d40c727f04bda06492d5df6
Matheus Brandino <m.v.brandino@gmail.com>
2018-07-16 18:16:14 -0300

    criacao do projeto com as dependencias

---
+++ src/main/java/br/com/matheus/moiptest/MoipTestApplication.java
@@ -0,0 +1,12 @@
+package br.com.matheus.moiptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoipTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoipTestApplication.class, args);
+    }
+}
