28bb1e04c9490934fed03eeb433d2f26d04dd097
Matheus Brandino <m.v.brandino@gmail.com>
2018-07-22 15:26:59 -0300

    removendo logs feios que não são tratados

--- src/main/java/br/com/matheus/moiptest/model/payment/method/PaymentMethodGenerator.java
+++ src/main/java/br/com/matheus/moiptest/model/payment/method/PaymentMethodGenerator.java
@@ -30,15 +30,10 @@ public class PaymentMethodGenerator {
             }
             return paymentMethod;
         } catch (InstantiationException e) {
-            e.printStackTrace();
         } catch (InvocationTargetException e) {
-            e.printStackTrace();
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         }
 
         return null;
