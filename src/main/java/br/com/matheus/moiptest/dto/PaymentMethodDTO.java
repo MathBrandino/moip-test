addd8fa3259264a381140a51bc2da6d99e03721c
Matheus Brandino <m.v.brandino@gmail.com>
2018-07-23 13:53:56 -0300

    criando construtores para facilitar o teste

--- src/main/java/br/com/matheus/moiptest/dto/PaymentMethodDTO.java
+++ src/main/java/br/com/matheus/moiptest/dto/PaymentMethodDTO.java
@@ -1,8 +1,8 @@
 package br.com.matheus.moiptest.dto;
 
-import br.com.matheus.moiptest.model.payment.method.PaymentMethodGenerator;
 import br.com.matheus.moiptest.model.payment.method.Card;
 import br.com.matheus.moiptest.model.payment.method.PaymentMethod;
+import br.com.matheus.moiptest.model.payment.method.PaymentMethodGenerator;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -17,6 +17,16 @@ public class PaymentMethodDTO {
     @Valid
     private Card card;
 
+
+    public PaymentMethodDTO(@NotNull BigDecimal amount, @NotNull String type, @Valid Card card) {
+        this.amount = amount;
+        this.type = type;
+        this.card = card;
+    }
+
+    @Deprecated
+    PaymentMethodDTO() {
+    }
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
